1570bf9c6700e0098da85ece5ad6dbd03259b018
Shah-Arsalan <shaharslan2278@gmail.com>
2022-06-06 21:38:56 +0530

    Added landing page

---
+++ src/LandingPage/LandingPage.jsx
@@ -0,0 +1,39 @@
+import "./LandingPage.css";
+import { useState } from "react";
+
+const LandingPage = () => {
+  const [name, setName] = useState("");
+  console.log(name);
+  return (
+    <>
+      <div className="name-input">
+        <div className="welcome-message">Hello there! Enter your name</div>
+        <input
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          type="text"
+          className=""
+          autoComplete="off"
+          autoFocus
+        />
+        {name.length > 0 && (
+          <button
+            onClick={() => localStorage.setItem("username", name)}
+            className="primary-button"
+          >
+            Enter Soothe{" "}
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export { LandingPage };
+
+// onClick={() => {
+//     localStorage.setItem("name", name);
+//     window.location.reload(false);
+// }}
